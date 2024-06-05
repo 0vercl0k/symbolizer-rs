4fbc99f385c18a60130735acd6133682336b3fb3
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2024-06-05 11:36:33 -0700

    some tests

--- crates/symbolizer/tests/basics.rs
+++ crates/symbolizer/tests/basics.rs
@@ -39,17 +39,31 @@ impl AddressSpace for RawAddressSpace {
 }
 
 #[test]
-fn foo() {
+fn raw() {
     let symcache = temp_dir().join("basics");
     let raw = RawAddressSpace::new(&r"c:\work\mrt100.raw").unwrap();
-    let modules = vec![Module::new("mrt100", 0x0, raw.len())];
+    let raw_len = raw.len();
+    let modules = vec![Module::new("mrt100", 0x0, raw_len)];
 
     let mut symb = Symbolizer::new(symcache, vec![], modules, raw);
 
-    let mut buf = Vec::new();
-    symb.full(&mut buf, 0x19_50).unwrap();
-    assert_eq!(
-        String::from_utf8(buf).unwrap(),
-        "mrt100!GetManagedRuntimeService+0x0"
-    );
+    let expected = [
+        (
+            0x19_50,
+            "mrt100!GetManagedRuntimeService+0x0",
+            "mrt100+0x00001950",
+        ),
+        (raw_len, "0x0000000000009000", "0x0000000000009000"),
+        (0xdeadbeef, "0x00000000deadbeef", "0x00000000deadbeef"),
+    ];
+
+    for (addr, expected_full, expected_modoff) in expected {
+        let mut full = Vec::new();
+        symb.full(addr, &mut full).unwrap();
+        assert_eq!(String::from_utf8(full).unwrap(), expected_full);
+
+        let mut modoff = Vec::new();
+        symb.modoff(addr, &mut modoff).unwrap();
+        assert_eq!(String::from_utf8(modoff).unwrap(), expected_modoff);
+    }
 }
